562bcc6c5a15ea1489b5ada8540d41c675a169b8
Naveen Kumar H <hgng1234@gmail.com>
2019-10-18 11:33:30 +0530

    commit

--- src/Components/Dashboard/Teacher/TEvents.js
+++ src/Components/Dashboard/Teacher/TEvents.js
@@ -1,11 +1,17 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import EventSync from "../Student/EventSync";
 
 const TEvents = ({ events }) => {
   return (
-    <div className="container">
+    <div className="container center">
+      <div className="btn white-text green ">
+        <Link className="white-text" to="/teventadd">
+          Add New Event
+        </Link>
+      </div>
+
       {events &&
         events.map(event => {
           return <EventSync event={event} />;
