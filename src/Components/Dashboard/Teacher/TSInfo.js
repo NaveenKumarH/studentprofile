1b005485835da18a71771726d89037f20fdbc01f
NaveenKumarH <hgng1234@gmail.com>
2019-10-19 10:14:01 +0530

    update

--- src/Components/Dashboard/Teacher/TSInfo.js
+++ src/Components/Dashboard/Teacher/TSInfo.js
@@ -27,6 +27,189 @@ export class TSInfo extends Component {
             Search
           </button>
         </div>
+        <div>
+          {this.props.academics.pre === undefined ? (
+            <div></div>
+          ) : this.props.academics.pre === "no" ? (
+            <div className="white-text">No data found</div>
+          ) : (
+            <div>
+              <div className="row s1 center">
+                <div className="col offset-s5 center">
+                  <div>Personal Information</div>{" "}
+                </div>
+              </div>
+              <div>
+                <div className="row s12">
+                  <div className="col s6">
+                    <div className="row s1">
+                      <div className="col s3">
+                        <div>Student Name </div>
+                      </div>
+                      <div className="col s1">
+                        <div>:</div>
+                      </div>
+                      <div className="col s3">
+                        <div>{this.props.academics.s.name}</div>
+                      </div>
+                    </div>
+
+                    <div className="row s1">
+                      <div className="col s3">
+                        <div htmlFor="email">Email </div>
+                      </div>
+                      <div className="col s1">
+                        <div>:</div>
+                      </div>
+                      <div className="col s2">
+                        <div>{this.props.academics.s.email}</div>
+                      </div>
+                    </div>
+
+                    <div className="row s1">
+                      <div className="col s3">
+                        <div htmlFor="date">D.O.B</div>
+                      </div>
+                      <div className="col s1">
+                        <div>:</div>
+                      </div>
+                      <div className="col s2">
+                        <div>{this.props.academics.s.dob}</div>
+                      </div>
+                    </div>
+
+                    <div className="row s1">
+                      <div className="col s3">
+                        <div>Address </div>
+                      </div>
+                      <div className="col s1">
+                        <div>:</div>
+                      </div>
+                      <div className="col s2">
+                        <div className="text">{this.props.academics.s.dob}</div>
+                      </div>
+                    </div>
+
+                    <div className="row s1">
+                      <div className="col s3">
+                        <div>Phone Number </div>
+                      </div>
+                      <div className="col s1">
+                        <div>:</div>
+                      </div>
+                      <div className="col s2">
+                        <div className="number">
+                          {this.props.academics.s.phno}
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="row s1">
+                      <div className="col s3">
+                        <div>Gender </div>
+                      </div>
+                      <div className="col s1">
+                        <div>:</div>
+                      </div>
+                      <div className="col s2">
+                        <div>{this.props.academics.s.gender}</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="row s1 center">
+                  <div>Skills</div>
+                </div>
+                <div className="row s1 ">
+                  <div className="col s2">Technical :</div>
+                </div>
+                <div className="row s1 ">
+                  <div className="col s2 offset-s1">Languages </div>
+                  <div className="col s1">
+                    <div>:</div>
+                  </div>{" "}
+                  <div className="col s2">
+                    <div>{this.state.plang}</div>
+                  </div>
+                </div>
+                <div className="row s1 ">
+                  <div className="col s2 offset-s1">Development Tools </div>
+                  <div className="col s1">
+                    <div>:</div>
+                  </div>
+                  <div className="col s2">
+                    <div>{this.state.devt}</div>
+                  </div>
+                </div>
+                <div className="row s1 ">
+                  <div className="col s2 offset-s1">Operating Systems </div>
+                  <div className="col s1">
+                    <div>:</div>
+                  </div>
+                  <div className="col s2">
+                    <div>{this.state.oss}</div>
+                  </div>
+                </div>
+                <div className="row s1 ">
+                  <div className="col s2 offset-s1">Databases </div>
+                  <div className="col s1">
+                    <div>:</div>
+                  </div>
+                  <div className="col s2">
+                    <div>{this.state.dbs}</div>
+                  </div>
+                </div>
+                <div className="row s1 ">
+                  <div className="col s2 ">Co-Curricular :</div>
+                  <div className="col s2">
+                    <div>{this.state.coc}</div>
+                  </div>
+                </div>
+                <div className="row s1 center">
+                  <div>Educational Information</div>
+                </div>
+                <div className="row s1 ">
+                  <div className="col s2"> Secondary</div>
+                  <div className="col s1">
+                    <div>:</div>
+                  </div>
+                  <div className="col s2">
+                    <div>
+                      <span>Board : </span>
+                      {this.state.secb}
+                    </div>
+                  </div>
+                  <div className="col s2">
+                    <div>
+                      <span>Percentage : </span>
+                      {this.state.secp}
+                    </div>
+                  </div>
+                </div>
+                <div className="row s1 ">
+                  <div className="col s2"> Higher Secondary</div>
+                  <div className="col s1">
+                    <div>:</div>
+                  </div>
+                  <div className="col s2">
+                    <div>
+                      <span>Board : </span>
+                      {this.state.highb}
+                    </div>
+                  </div>
+                  <div className="col s2">
+                    <div>
+                      <span>Percentage : </span>
+                      {this.state.highp}
+                    </div>
+                  </div>
+                </div>
+                }
+              </div>
+            </div>
+          )}
+          ;
+        </div>
       </div>
     );
   }
