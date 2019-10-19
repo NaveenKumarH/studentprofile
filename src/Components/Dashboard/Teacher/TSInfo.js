dfca71fbd076dd642e89dda2a2654e4bb1174861
NaveenKumarH <hgng1234@gmail.com>
2019-10-19 09:45:34 +0530

    updates

--- src/Components/Dashboard/Teacher/TSInfo.js
+++ src/Components/Dashboard/Teacher/TSInfo.js
@@ -1,10 +1,34 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import sds from "../../../store/Actions/SDS";
 export class TSInfo extends Component {
+  state = {};
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+  handleClick = e => {
+    e.preventDefault();
+    this.props.search(this.state);
+  };
   render() {
-    return <div className="container"></div>;
+    return (
+      <div className="container">
+        <div className="row input-field">
+          <label>Student Reg No.</label>
+          <input
+            className="white"
+            type="text"
+            id="srn"
+            onChange={this.handleChange}
+          ></input>
+          <button className="btn green white-text" onClick={this.handleClick}>
+            Search
+          </button>
+        </div>
+      </div>
+    );
   }
 }
 const mapstate = state => {
